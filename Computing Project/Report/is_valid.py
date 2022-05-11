16d425fd67d79b411256b57510af4d420746519d
Fahim <105354222+Fah1mA@users.noreply.github.com>
2022-05-11 11:07:27 +0100

    Add files via upload

---
+++ Computing Project/Report/is_valid.py
@@ -0,0 +1,6 @@
+def is_valid(data, length):
+    if length == data:
+        return True
+is_valid(6, 6)
+    #
+
---
+++ Computing Project/Report/range_validation.py
@@ -0,0 +1,5 @@
+def validation_check(data, hi, lo):
+    if data in range(lo, hi):
+        return True
+
+validation_check(3, 5, 1)
